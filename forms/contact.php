35de99a5d7b5419fab37647e20b8efef04353c91
Mateusz Stankiewicz <62524229+matthewstanki@users.noreply.github.com>
2025-04-28 01:34:54 +0200

    Add files via upload

--- forms/contact.php
+++ forms/contact.php
@@ -1,32 +1,22 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $to = "mateuszstankiewicz999@gmail.com";
+  $subject = htmlspecialchars($_POST["subject"]);
+  $name = htmlspecialchars($_POST["name"]);
+  $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+  $message = htmlspecialchars($_POST["message"]);
 
-$receiving_email_address = 'mateuszstankiewicz999@gmail.com';
+  $headers = "From: $name <$email>\r\n";
+  $headers .= "Reply-To: $email\r\n";
+  $headers .= "Content-Type: text/plain; charset=utf-8\r\n";
 
-if (file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php')) {
-  include($php_email_form);
+  $full_message = "Imię: $name\nEmail: $email\n\nWiadomość:\n$message";
+
+  if (mail($to, $subject, $full_message, $headers)) {
+    echo "OK";
+  } else {
+    echo "Błąd przy wysyłaniu wiadomości.";
+  }
 } else {
-  die('Unable to load the "PHP Email Form" Library!');
+  echo "Nieprawidłowe żądanie.";
 }
-
-$contact = new PHP_Email_Form;
-$contact->ajax = true;
-
-$contact->to = $receiving_email_address;
-$contact->from_name = $_POST['name'];
-$contact->from_email = $_POST['email'];
-$contact->subject = $_POST['subject'];
-
-$contact->smtp = array(
-  'host' => 'example.com',
-  'username' => 'example',
-  'password' => 'pass',
-  'port' => '587'
-);
-
-$contact->add_message($_POST['name'], 'From');
-$contact->add_message($_POST['email'], 'Email');
-$contact->add_message($_POST['message'], 'Message', 10);
-
-echo $contact->send();
-
-  // soon
